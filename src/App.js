e0ba31fca394e3cabe88588ce03f214f9874c13e
botketan <ketansingh918@gmail.com>
2024-01-21 01:07:53 +0530

    Router bug

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import { BrowserRouter as Router,Route, Routes, useLocation } from 'react-router-dom';
 import Navbar from './Components/Navbar.js';
 import Home from './Components/Home.js';
 function App() {
