88a0b0b3464e6eff9ecd34bdd3af5463a8d55708
botketan <ketansingh918@gmail.com>
2024-01-21 01:14:15 +0530

    heading Changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import './App.css';
 import Navbar from './Components/Navbar.js';
 import Home from './Components/Home.js';
+import { useEffect } from 'react';
 function App() {
+  useEffect (() => {
+    document.title = "Kriti2024";
+  }, []);
   return (
       <div>
          <Navbar/>
