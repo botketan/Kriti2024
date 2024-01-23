fc6cbdabcfae8a3dabd3739fd8c389561a2f99f4
botketan <ketansingh918@gmail.com>
2024-01-23 14:06:01 +0530

    mobile changes

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,11 +1,11 @@
 const  Navbar=()=> {
     return(
-        <div className="w-[100%] flex flex-row flex-wrap h-[10vh] justify-between mx-5 px-[3vw] my-[2vh] content-center">
-                <img src={require('../assests/Techboardlogo.png')} alt="logo" className="w-[5vw] "/>
-            <p className="font-gt text-2xl md:text-4xl lg:text-6xl text-black my-[10px]">
+        <div className="md:w-[100%] lg:w-[100%] flex flex-row flex-wrap h-[10vh] justify-between mx-5 px-[3vw] my-[4vh] content-center">
+                <img src={require('../assests/Techboardlogo.png')} alt="logo" className="w-auto  max-w-[60px] md:max-w-[100px] lg:max-w-[100px]"/>
+            <p className="font-gt text-4xl md:text-4xl lg:text-6xl text-black my-[10px]">
                 Kriti 24'
             </p>
-            <button className="text-white p-1 m-1 bg-[#502515] w-[8vw] text-xl h-[8vh] rounded-lg">
+            <button className="text-white p-1 m-1 bg-[#502515] w-[25vw] md:w-[8vw] lg:w-[8vw] text-xl h-[8vh] rounded-lg ">
                 Login
             </button>
         </div>
