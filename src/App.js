6acfbc0b97c057eb0d0dc610c8fbb718f4537899
DeveshKamble <122542735+DeveshKamble@users.noreply.github.com>
2024-01-29 23:19:05 +0530

    Revert "Added Registration Page"

--- src/App.js
+++ src/App.js
@@ -2,21 +2,14 @@ import './App.css';
 import Navbar from './Components/Navbar.js';
 import Home from './Components/Home.js';
 import { useEffect } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Registration from './Components/Registration.js';
 function App() {
   useEffect (() => {
     document.title = "Kriti2024";
   }, []);
   return (
       <div>
-        <BrowserRouter>
-          <Navbar/>
-          <Routes>
-            <Route path='/' element={<Home/>} />
-            <Route path = "/:num/:name" element={<Registration />} /> {/* The links for registration pages will be in the card of home page */}
-          </Routes>
-        </BrowserRouter>
+         <Navbar/>
+         <Home/>
       </div>
        
   );
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,5 +1,3 @@
-import { Link } from 'react-router-dom';
-
 const  Navbar=()=> {
     return(
         <div className="md:w-[100%] lg:w-[100%] flex flex-row flex-wrap h-[10vh] justify-between mx-5 px-[3vw] my-[4vh] content-center">
@@ -7,12 +5,9 @@ const  Navbar=()=> {
             <p className="font-gt text-4xl md:text-4xl lg:text-6xl text-black my-[10px]">
                 Kriti 24'
             </p>
-            
-            <Link>
-                <button className="text-white p-1 m-1 bg-[#502515] w-[25vw] md:w-[8vw] lg:w-[8vw] text-xl h-[8vh] rounded-lg ">
-                    Login
-                </button>
-            </Link>
+            <button className="text-white p-1 m-1 bg-[#502515] w-[25vw] md:w-[8vw] lg:w-[8vw] text-xl h-[8vh] rounded-lg ">
+                Login
+            </button>
         </div>
     )
 }
