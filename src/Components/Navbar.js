e88374f8179de32adb6fb17118c9504e4a47b106
botketan <ketansingh918@gmail.com>
2024-01-30 19:21:55 +0530

    Minor Change

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -2,8 +2,10 @@ import { Link } from 'react-router-dom';
 
 const  Navbar=({title,link})=> {
     return(
+        
         <div className="md:w-[100%] lg:w-[100%] flex flex-row flex-wrap h-[10vh] justify-between mx-5 px-[3vw] my-[4vh] content-center">
-                <img src={require('../assests/Techboardlogo.png')} alt="logo" className="w-auto  max-w-[60px] md:max-w-[100px] lg:max-w-[100px]"/>
+                <Link to ="/"><img src={require('../assests/Techboardlogo.png')} alt="logo" className="w-auto  max-w-[60px] md:max-w-[100px] lg:max-w-[100px]"/>
+                </Link>
             <p className="font-gt text-4xl md:text-4xl lg:text-6xl text-black my-[10px]">
                 Kriti 24'
             </p>
